885e05053343ac7e5920b3b6f02fa4d0b60bbda5
Muzamil5678 <169040633+Muzamil5678@users.noreply.github.com>
2024-05-06 14:39:16 +0500

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float a,b,add,sub,mul,div;
+	cout<<"please enter value 1:";
+	cin>>a;
+	cout<<"please enter value 2:";
+	cin>>b;
+	add=a+b;
+	cout<<"The addition is :"<<add;
+	sub=a-b;
+	cout<<"The subtraction is :"<<sub;
+	mul=a*b;
+	cout<<"The multiplication is :"<<mul;
+	div=a/b;
+	cout<<"The division of two number is :"<<div;
+}
+
+	
